cda206b6554cbe202a284137927e9d2c998eb531
AharonDL <aharon@dataloop.ai>
2020-10-14 17:41:11 +0300

    fifth

---
+++ main.py
@@ -0,0 +1,18 @@
+import dtlpy as dl
+import logging
+from .package import Printer
+
+logger = logging.getLogger(name=__name__)
+
+
+class ServiceRunner(dl.BaseServiceRunner):
+    """
+    Package runner class
+
+    """
+    def __init__(self):
+        self.printer = Printer()
+
+    def run(self):
+        self.printer.print()
+        print('This is 5th commit')
---
+++ package.py
@@ -0,0 +1,6 @@
+class Printer:
+    def __init__(self):
+        self.message = '########### Hello World ###########'
+
+    def print(self):
+        print(self.message)
